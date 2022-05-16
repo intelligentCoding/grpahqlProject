deaf8d708ab3a19c760bc5eb02a6807f2d79ee2a
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-05-16 16:05:17 -0600

    login resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,13 +1,37 @@
 import { User } from "../entities/User";
 import { Mycontext } from "src/types";
 import argon2 from "argon2";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 @InputType()
 class UserNamePasswordInput {
   @Field()
   username: string;
   @Field()
   password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 @Resolver()
 export class UserResolver {
@@ -16,6 +40,29 @@ export class UserResolver {
     @Arg("options", () => UserNamePasswordInput) options: UserNamePasswordInput,
     @Ctx() { em }: Mycontext
   ) {
+    // validate username
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "very short user name",
+          },
+        ],
+      };
+    }
+
+    // validate password
+    if (options.password.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "login",
+            message: "invalid login",
+          },
+        ],
+      };
+    }
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
@@ -25,5 +72,50 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UserNamePasswordInput,
+    @Ctx() { em }: Mycontext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {
+      username: options.username.toLowerCase(),
+    });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "User Name not found",
+          },
+        ],
+      };
+    }
+
+    try {
+      const valid = await argon2.verify(user.password, options.password);
+      if (!valid)
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "login not valid",
+            },
+          ],
+        };
+    } catch (error) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "login not valid",
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
