96a1b83bb75e6a16f1fdb16d193807d24c208974
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-05-16 12:06:23 -0600

    graphql hello world

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import microConfig from './mikro-orm.config';
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+
   //automatically run migrations before anything else
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, {
-    title: "My Title"
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
-  orm.em.persistAndFlush(post);
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
