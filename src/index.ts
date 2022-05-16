d937f2fb53bdcb7c856c4df3e91324d3bd66a4a5
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-05-16 11:28:59 -0600

    run first migration

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import microConfig from './mikro-orm.config';
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "graphQL",
-    user: "postgres",
-    password: "kasjee",
-    type: "postgresql",
-    debug: !__prod__,
-    allowGlobalContext: true,
-  });
+  const orm = await MikroORM.init(microConfig);
 
   const post = orm.em.create(Post, {
     title: "My Title",
