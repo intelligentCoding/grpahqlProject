1c1637507d725f53092e97d200c35fad8b7ce98f
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-05-16 12:20:23 -0600

    Post setup with graphql

--- dist/resolvers/post.js
+++ dist/resolvers/post.js
@@ -8,20 +8,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PostResolver = void 0;
+const Post_1 = require("../entities/Post");
 const type_graphql_1 = require("type-graphql");
 let PostResolver = class PostResolver {
-    hello() {
-        return "hello world";
+    posts({ em }) {
+        return em.find(Post_1.Post, {});
     }
 };
 __decorate([
-    (0, type_graphql_1.Query)(() => [Postt]),
+    (0, type_graphql_1.Query)(() => [Post_1.Post]),
+    __param(0, (0, type_graphql_1.Ctx)()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
-    __metadata("design:returntype", void 0)
-], PostResolver.prototype, "hello", null);
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], PostResolver.prototype, "posts", null);
 PostResolver = __decorate([
     (0, type_graphql_1.Resolver)()
 ], PostResolver);
