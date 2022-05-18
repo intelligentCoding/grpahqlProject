e8b204f8b63c03987e3533d5de2061b6431b6326
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-05-18 09:08:55 -0600

    refactor input

---
+++ src/components/Input.tsx
@@ -0,0 +1,25 @@
+import React, { InputHTMLAttributes } from "react";
+import { useField } from "formik";
+import { FormControl, FormLabel, Input, FormErrorMessage, Textarea} from "@chakra-ui/core";
+
+type CustomInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+  textarea?: boolean;
+};
+
+export const CustomInput: React.FC<CustomInputProps> = ({
+  label, textarea, size, ...props }) => {
+  let InputOrTextarea = Input;
+  if (textarea) {
+    InputOrTextarea = Textarea;
+  }
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <InputOrTextarea {...field} {...props} id={field.name} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
