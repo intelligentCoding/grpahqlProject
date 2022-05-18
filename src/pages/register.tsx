0adc75af776a99dcf22eddb5d6538e4492eb8ede
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-05-18 08:38:34 -0600

    Register page initialized

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Formik, Form } from "formik";
+import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
+import { format } from 'path';
 interface registerProps {
 
 }
 
 export const Register: React.FC<registerProps> = ({}) => {
     return (
-        <Formik>
-            {() => (
+        <Formik initialValues={{userN: "", pass: ""}} onSubmit={(values) => {
+            console.log(values);
+        }}>
+            {(values, handleChange) => (
                 <Form>
-                    <div>hello </div>
+                    <FormControl>
+                        <FormLabel htmlFor='userN'>userName</FormLabel>
+                        <Input 
+                            value={values.userN}
+                            onChange={handleChange}
+                            id="userN"
+                            placeholder='Enter User Name'
+                        />
+                        {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+                    </FormControl>
                 </Form>
             )}
         </Formik>
