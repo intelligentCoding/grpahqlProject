f514b59322b54f450f05d2fb490857a27f90e487
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-05-18 08:48:08 -0600

    register page setup and page wrapper created

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import { Formik, Form } from "formik";
 import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
-import { format } from 'path';
+import { PageWrapper } from "../components/PageWrapper"
 interface registerProps {
 
 }
 
 export const Register: React.FC<registerProps> = ({}) => {
     return (
-        <Formik initialValues={{userN: "", pass: ""}} onSubmit={(values) => {
-            console.log(values);
-        }}>
-            {(values, handleChange) => (
-                <Form>
-                    <FormControl>
-                        <FormLabel htmlFor='userN'>userName</FormLabel>
-                        <Input 
-                            value={values.userN}
-                            onChange={handleChange}
-                            id="userN"
-                            placeholder='Enter User Name'
-                        />
-                        {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-                    </FormControl>
-                </Form>
-            )}
-        </Formik>
+        <PageWrapper>
+            <Formik initialValues={{userN: "", pass: ""}} onSubmit={(values) => {
+                console.log(values);
+            }}>
+                {(values, handleChange) => (
+                    <Form>
+                        <FormControl>
+                            <FormLabel htmlFor='userN'>userName</FormLabel>
+                            <Input 
+                                value={values.userN}
+                                onChange={handleChange}
+                                id="userN"
+                                placeholder='Enter User Name'
+                            />
+                            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+                        </FormControl>
+                    </Form>
+                )}
+            </Formik>
+        </PageWrapper>
     );
 }
 //in next.js have to export default component
